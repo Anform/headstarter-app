8f82077f1b286cb2772eaff49aadb58a054c9f38
Robert Balatbat <73982521+robjgb@users.noreply.github.com>
2022-08-17 21:43:40 -0700

    added join zoom meeting in video call

--- src/components/VideoCall.jsx
+++ src/components/VideoCall.jsx
@@ -1,63 +1,105 @@
-import React, {useEffect, useRef, useState} from 'react'
-import WebcamContainer from './WebcamContainer';
+import React, {useRef} from 'react';
+import ZoomMtgEmbedded from '@zoomus/websdk/embedded';
 import './videocall.css'
 
 function VideoCall() {
-  const [isLoaded, setIsLoaded] = useState(false);
-  let videoRef = useRef();
-  const servers = {
-    iceServers: [
-        {
-        urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
-        },
-    ],
-    iceCandidatePoolSize: 10,
-    };
+  const zoomIDRef = useRef(null);
+  const zoomPWRef = useRef(null);
+  const client = ZoomMtgEmbedded.createClient();
 
-  const pc = new RTCPeerConnection(servers);
-  const [localStream, setLocalStream] = useState(null);
-  let remoteStream = null;
+  // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
+  var signatureEndpoint = 'https://headstarter-group-zoom-sign.herokuapp.com/'
+  // This Sample App has been updated to use SDK App type credentials https://marketplace.zoom.us/docs/guides/build/sdk-app
+  var sdkKey = 'Hg458B5SMFNbxhjMIOSsUivexrqX8C3Fssx1'
+  var meetingNumber = ''
+  var role = 0
+  var userName = 'React'
+  var userEmail = ''
+  var passWord = ''
+  // pass in the registrant's token if your meeting or webinar requires registration. More info here:
+  // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/meetings#join-registered
+  // Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/webinars#join-registered
+  var registrantToken = ''
 
-  async function handleWebcam(){
-    setLocalStream(await navigator.mediaDevices.getUserMedia({ video: true, audio: true }));
-    let remoteStream = new MediaStream();
+  function getSignature(e) {
+    e.preventDefault();
+    meetingNumber = zoomIDRef.current.value;
+    passWord = zoomPWRef.current.value;
 
-    localStream.getTracks().forEach((track) => {
-      pc.addTrack(track, localStream);
+    fetch(signatureEndpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        meetingNumber: meetingNumber,
+        role: role
+      })
+    }).then(res => res.json())
+    .then(response => {
+      startMeeting(response.signature)
+    }).catch(error => {
+      console.error(error)
     })
-    // Pull tracks from remote stream, add to video stream
-    pc.ontrack = (event) => {
-      event.streams[0].getTracks().forEach((track) => {
-        remoteStream.addTrack(track);
-      });
-    };
   }
 
-  useEffect(()=> {
-    videoRef.current.srcObject = localStream;
-  }, [localStream])
+  function startMeeting(signature) {
 
-  const connections = ["test", "test", "test"]
+    let meetingSDKElement = document.getElementById('meetingSDKElement');
+
+    client.init({
+      debug: true,
+      zoomAppRoot: meetingSDKElement,
+      language: 'en-US',
+      customize: {
+        meetingInfo: ['topic', 'host', 'mn', 'pwd', 'telPwd', 'invite', 'participant', 'dc', 'enctype'],
+        toolbar: {
+          buttons: [
+            {
+              text: 'Custom Button',
+              className: 'CustomButton',
+              onClick: () => {
+                console.log('custom button');
+              }
+            }
+          ]
+        }
+      }
+    });
+
+    client.join({
+    	sdkKey: sdkKey,
+    	signature: signature,
+    	meetingNumber: meetingNumber,
+    	password: passWord,
+    	userName: userName,
+      userEmail: userEmail,
+      tk: registrantToken
+    })
+  }
 
   return (
-    <>
-    <div className='controls'>
-    <button className='webcam-button' onClick={handleWebcam}> Start Webcam </button>
-    <button className='create-call-button'> Create a new call </button>
-    <button className='join-call-button'> Join a call </button>
+    <div className="App">
+      <main>
+        <h1>Join a Zoom Meeting</h1>
+        {/* For Component View */}
+        <div id="meetingSDKElement">
+          {/* Zoom Meeting SDK Component View Rendered Here */}
+        </div>
+        <div>
+          <form>
+            <label>
+              Zoom ID:
+             <input type="text" placeholder='Enter Zoom ID' ref={zoomIDRef} required/>
+            </label>
+            <label>
+              Meeting Password:
+             <input type="text" placeholder='Enter Password' ref={zoomPWRef} required/>
+            </label>
+            <input type="submit" onClick={getSignature} value="Join Meeting" />
+          </form>
+        </div>
+      </main>
     </div>
-    <div className='all-webcams'>
-    <WebcamContainer stream={videoRef}/>
-      {
-        connections.map((index)=>(
-          <WebcamContainer stream={null}/>
-        )) 
-      }
-    </div>
-
-    
-    </>
-  )
+  );
 }
 
 export default VideoCall
